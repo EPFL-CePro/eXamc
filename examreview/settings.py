870bfa3be3d472bf5bf04295343aa7bc4dfaf019
Ludovic Bonivento <ludovic.bonivento@epfl.ch>
2022-01-05 16:51:01 +0100

    upd

--- examreview/settings.py
+++ examreview/settings.py
@@ -134,6 +134,10 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static/'
 
+# Scans folder
+SCAN_ROOT = BASE_DIR / 'scan/'
+SCAN_URL = '/scan/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
