dc8677bed9e05712a3cf68494bb8b67d376dedbe
lucile <lucile.pinard@gmail.com>
2024-05-08 14:42:10 +0200

    test and doc

--- docs/source/modules/review/review-settings-pages-groups.rst
+++ docs/source/modules/review/review-settings-pages-groups.rst
@@ -2,3 +2,16 @@ Pages groups
 ------------
 
 Review Settings - Pages Groups
+
+It is composed by a "+" button, a "save" button and a table with many entries.
+The first one, "GROUP NAME", is dedicated to give the name of the question.
+The next two entry are used to apply the numbers of pages that contain the question.
+
+The last entries in the tree are buttons.The first one, "DELETE", is used to remove the question.
+The next one is "GRADING HELP" for leaving comments on how correct the question is.
+The last one, is dedicated to detect the corrector boxes.
+
+After making changes, use the "save" button.
+
+.. image:: images/review_pages_groups.png
+   :width: 600
--- examc_app/tests/test_forms.py
+++ examc_app/tests/test_forms.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
-from examc_app.forms import UploadScansForm, ManagePagesGroupsForm, ManageReviewersForm, ExportMarkedFilesForm
+from examc_app.forms import *
 from examc_app.models import *
+
 
 class FormsTestCase(TestCase):
     def test_upload_scans_form(self):
@@ -26,5 +27,41 @@ class FormsTestCase(TestCase):
     def test_export_marked_files_form(self):
         form = ExportMarkedFilesForm()
         self.assertEqual(form.fields['export_type'].widget.__class__.__name__, 'RadioSelect')
-        self.assertEqual(form.fields['export_type'].choices, [(1, 'JPGs (one per page)'), (2, 'PDFs (one per student copy)')])
+        self.assertEqual(form.fields['export_type'].choices,
+                         [(1, 'JPGs (one per page)'), (2, 'PDFs (one per student copy)')])
+
+    def test_login_pages_form(self):
+        form = LoginForm()
+        self.assertIsInstance(form.fields['username'], forms.CharField)
+        self.assertIsInstance(form.fields['password'], forms.CharField)
+        self.assertEqual(form.fields['username'].max_length, 65)
+        self.assertEqual(form.fields['password'].max_length, 65)
+        self.assertIsInstance(form.fields['password'].widget, forms.PasswordInput)
+
+    def test_export_results_form(self):
+        form = ExportResultsForm()
+
+        self.assertIn('exportIsaCsv', form.fields)
+        self.assertIn('exportExamScalePdf', form.fields)
+        self.assertIn('exportStudentsDataCsv', form.fields)
+        self.assertIn('scale', form.fields)
+
+        self.assertIsInstance(form.fields['exportIsaCsv'], forms.BooleanField)
+        self.assertIsInstance(form.fields['exportExamScalePdf'], forms.BooleanField)
+        self.assertIsInstance(form.fields['exportStudentsDataCsv'], forms.BooleanField)
+        self.assertIsInstance(form.fields['scale'], forms.ChoiceField)
+
+        self.assertEqual(form.fields['exportIsaCsv'].label, 'export ISA .csv')
+        self.assertEqual(form.fields['exportExamScalePdf'].label, 'export Exam scale pdf')
+        self.assertEqual(form.fields['exportStudentsDataCsv'].label, 'export Students data .csv')
+
+        self.assertIsInstance(form.fields['exportIsaCsv'].widget, forms.CheckboxInput)
+        self.assertIsInstance(form.fields['exportExamScalePdf'].widget, forms.CheckboxInput)
+        self.assertIsInstance(form.fields['exportStudentsDataCsv'].widget, forms.CheckboxInput)
+        self.assertIsInstance(form.fields['scale'].widget, forms.RadioSelect)
+
+        self.assertEqual(form.fields['exportIsaCsv'].widget.attrs['class'], 'form-check-input')
+        self.assertEqual(form.fields['exportExamScalePdf'].widget.attrs['class'], 'form-check-input')
+        self.assertEqual(form.fields['exportStudentsDataCsv'].widget.attrs['class'], 'form-check-input')
+        self.assertEqual(form.fields['scale'].widget.attrs['class'], 'custom-radio-list form-check-inline')
 
