db73df2e0a69a5821923703899b1442fb54ce7fb
Ludovic Bonivento <ludovic.bonivento@epfl.ch>
2024-10-23 08:43:04 +0200

    wsgi and manage.py update

--- examc/wsgi.py
+++ examc/wsgi.py
@@ -5,12 +5,18 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/wsgi/
+
+Change 'INSTANCE' with the instance to use and the related files (settings and .env)
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
+from dotenv import load_dotenv
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'examc.settings')
+dotenv_path = os.path.join(os.path.dirname(__file__),'../.env.local')
+load_dotenv(dotenv_path)
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'examc.settings.local')
 
 application = get_wsgi_application()
