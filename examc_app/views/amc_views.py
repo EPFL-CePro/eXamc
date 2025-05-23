0c0893605d0f97ea62e70b375886827b664cabf5
Ludovic Bonivento <ludovic.bonivento@epfl.ch>
2025-05-23 16:35:44 +0200

    error correction

--- examc_app/views/amc_views.py
+++ examc_app/views/amc_views.py
@@ -558,7 +558,7 @@ def get_amc_scan_url(request,exam_pk):
             print("********* path ********** " + scan_path + " **************************")
             app_home_path = str(settings.BASE_DIR).replace(str(Path.home()), '%HOME')
             print("********* app_home_path ********** " + app_home_path + " **************************")
-            scan_path = scan_path.replace(app_home_path,'')
+            scan_path = scan_path.replace(app_home_path+'/','')
 
             print("********* path ********** " + scan_path + " **************************")
 
