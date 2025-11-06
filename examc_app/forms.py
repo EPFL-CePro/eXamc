9c7e057a42680c7a5055eb71fedef022958e8eeb
Azecko <samikarin24@gmail.com>
2025-11-06 12:24:08 +0100

    [feature] Special room plan
    TL;DR This is the script [room_plan](https://github.com/theGoldenKiwi2/room_plan) as a web app.
    
    - Pills `Default` and `Special` on the room plan page
    - New route `/generate_room_plan/special`
    - Select a room image from `rooms_plans/map` using a dropdown
    - Select the numbering option between `Numbers`, `Letters` or `Prefix` (an input will be shown to select a prefix`
    - Download to JPG with the `Download` button
    - Undo the last placed point with the `Undo` button
    
    The `Numbering option` and the `Special file` have been removed from the "Default" SeatingForm.

--- examc_app/forms.py
+++ examc_app/forms.py
@@ -199,24 +199,6 @@ class SeatingForm(forms.Form):
         )
     )
 
-    numbering_option = forms.ChoiceField(
-        choices=[('continuous', 'Continuous'), ('special', 'Special')],
-        label='Numbering Option',
-        help_text="Select how seats are numbered. The special option is used for student needs. Upload a .csv with "
-                  "the special numbers. Fill in the first and last number then download. The numbers will be the one "
-                  "that are in the special file.",
-        widget=forms.RadioSelect(attrs={'onchange': "showHideSpecialFile(this.value);"}),
-        initial='continuous'
-    )
-
-    skipping_option = forms.ChoiceField(
-        choices=[('noskip', 'No skip'), ('skip', 'Skip')],
-        label='Skip Option',
-        help_text="Choose whether to skip seats. Upload a .csv with the numbers to skip.",
-        widget=forms.RadioSelect(attrs={'onchange': "showHideSpecialFile(this.value)", 'id': 'id_skipping_option'}),
-        initial='noskip'
-    )
-
     fill_all_seats = forms.BooleanField(
         required=False,
         help_text="Fill all seats of the plans from the first number to the end of the plan.",
@@ -237,13 +219,6 @@ class SeatingForm(forms.Form):
         required=False
     )
 
-    special_file = forms.FileField(
-        label='Special File',
-        required=False,
-        help_text="Upload a file for special seat numbers or skipping. A CSV file with all the numbers you want to skip or add.",
-        widget=forms.ClearableFileInput(attrs={'id': 'id_special_file'})
-    )
-
     shape_to_draw = forms.ChoiceField(
         choices=[('circle', 'Circle'), ('square', 'Square')],
         label='Shape to Draw',
@@ -258,6 +233,33 @@ class SeatingForm(forms.Form):
     #     widget=forms.RadioSelect(attrs={'data-tooltip': "Choose the skipping numbers."}),
     #     initial='0'
     # )
+
+JPG_DIR = str(settings.ROOMS_PLANS_ROOT) + "/map/"
+IMAGE_FILES = sorted([(f, f) for f in os.listdir(JPG_DIR) if f.endswith('.jpg')])
+class SeatingSpecialForm(forms.Form):
+    img_file = forms.ChoiceField(
+        choices=IMAGE_FILES,
+        label='Room',
+        help_text="Select one or more rooms. The room order is the alphabetic one",
+        widget=forms.Select(
+            attrs={
+                "data-tooltip-location": "top",
+                'id': 'id_img_file',
+                'class': "selectpicker form-control",
+                'size': 5,
+                'data-live-search': "true",
+                'onchange': "onRoomChange(this.value);"
+            }
+        )
+    )
+
+    numbering_option = forms.ChoiceField(
+        choices=[('numbers', 'Numbers'), ('letters', 'Letters'), ('prefix', 'Prefix')],
+        label='Numbering Option',
+        help_text="Select how seats are numbered. The `Prefix` option lets you choose the prefix before a number. Ex : `MyPrefix-13`",
+        widget=forms.RadioSelect(),
+        initial='numbers'
+    )
 
 
 class ldapForm(forms.Form):
