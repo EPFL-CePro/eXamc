5d7beb0f990ac4eb3ca99ae46aca2b8a01c33e35
Ludovic Bonivento <ludovic.bonivento@epfl.ch>
2025-11-06 15:13:51 +0100

    update nginx conf to allow big file upload

--- deploy/gunicorn.conf.py
+++ deploy/gunicorn.conf.py
@@ -1,11 +1,11 @@
 # examc/deploy/gunicorn.conf.py
 import multiprocessing, os
 bind = "0.0.0.0:8000"
-workers = int(os.environ.get("WEB_CONCURRENCY", multiprocessing.cpu_count()*2 + 1))
+workers = int(os.environ.get("WEB_CONCURRENCY", max(2,multiprocessing.cpu_count())))
 threads = int(os.environ.get("GUNICORN_THREADS", 1))
-timeout = int(os.environ.get("WEB_TIMEOUT", 60))
-graceful_timeout = 30
-keepalive = 5
+timeout = int(os.environ.get("WEB_TIMEOUT", 3600))
+graceful_timeout = 60
+keepalive = 75
 max_requests = 1000
 max_requests_jitter = 100
 worker_tmp_dir = "/dev/shm"
