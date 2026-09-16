0215c643d73fb0c7b0d12b92cdecf3d9485bb2e0
Alexandre Zürcher <alexandre@zurcher.digital>
2026-09-16 15:40:46 +0200

    examc_app - removed serializers.py and viewsets.py (will be implemented in new modules)

--- app/examc_app/api/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from examc_app.models import Exam
-
-
-class ExamSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Exam
-        fields = ["code", "name", "date"]
--- app/examc_app/api/viewsets.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import viewsets
-
-from examc_app.api.serializers import ExamSerializer
-from examc_app.models import Exam
-
-
-class ExamViewSet(viewsets.ModelViewSet):
-    queryset = Exam.objects.all()
-    serializer_class = ExamSerializer
